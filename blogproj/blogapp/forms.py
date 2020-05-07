30be1852fa381afa9cd13da31bf75a6832900740
usermuser <lwrdrstv@gmail.com>
2020-05-07 22:38:36 +0300

    dev: removed wrong statement

--- blogproj/blogapp/forms.py
+++ blogproj/blogapp/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 from .models import Post
 
-class PostForm(forms.ModelForm):
-    template_name = 'blogapp/'
 
+class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = '__all__'
